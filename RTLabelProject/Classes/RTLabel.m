165f1d470155a49833ba74ed63023452c567baa7
Alan Rogers <alanjrogers@me.com>
2016-10-05 11:09:17 +1100

    LOL

--- RTLabelProject/Classes/RTLabel.m
+++ RTLabelProject/Classes/RTLabel.m
@@ -925,7 +925,7 @@
 						NSString *value = [[pair subarrayWithRange:NSMakeRange(1, [pair count] - 1)] componentsJoinedByString:@"="];
 						if (value.length > 0) {
 								value = [value stringByReplacingOccurrencesOfString:@"\"" withString:@"" options:NSLiteralSearch range:NSMakeRange(0, 1)];
-								if (value.lenght > 0) {
+								if (value.length > 0) {
 									value = [value stringByReplacingOccurrencesOfString:@"\"" withString:@"" options:NSLiteralSearch range:NSMakeRange([value length]-1, 1)];
 								}
 						}
